4c5d76873e2d0cbc66c8cc713440fb389c06f78c
Evann9 <bluefrogg9@gmail.com>
2026-03-19 21:43:54 +0900

    오류 수정

--- pro5anal/pandasquiz.py
+++ pro5anal/pandasquiz.py
@@ -1,43 +1,43 @@
 import numpy as np
 import pandas as pd
 from pandas import Series, DataFrame
-# # 1)
-# #   a) 표준정규분포를 따르는 9 X 4 형태의 DataFrame을 생성하시오. 
-# data = pd.DataFrame(np.random.randn(9,4))
-# print(data)
+# 1)
+#   a) 표준정규분포를 따르는 9 X 4 형태의 DataFrame을 생성하시오. 
+data = pd.DataFrame(np.random.randn(9,4))
+print(data)
 
-# #   b) a에서 생성한 DataFrame의 칼럼 이름을 - No1, No2, No3, No4로 지정하시오
-# data.columns = ['No1','No2','No3','No4']
-# print(data)
+#   b) a에서 생성한 DataFrame의 칼럼 이름을 - No1, No2, No3, No4로 지정하시오
+data.columns = ['No1','No2','No3','No4']
+print(data)
 
-# #   c) 각 컬럼의 평균을 구하시오. mean() 함수와 axis 속성 사용
-# print(data.mean(axis=0))
+#   c) 각 컬럼의 평균을 구하시오. mean() 함수와 axis 속성 사용
+print(data.mean(axis=0))
 
-# print()
-# # 2)
-# # a) DataFrame으로 위와 같은 자료를 만드시오. colume(열) name은 numbers, row(행) name은 a~d이고 값은 10~40.
-# numbers = pd.DataFrame(np.arange(10,50,10), columns=['numbers'], index=['a','b','c','d'])
-# print(numbers)
-# # b) c row의 값을 가져오시오.
-# print(numbers.loc["c"])
-# # c) a, d row들의 값을 가져오시오.
-# print(numbers.iloc[0:4:3])
-# # d) numbers의 합을 구하시오.
-# print()
-# print(numbers.sum())
-# # e) numbers의 값들을 각각 제곱하시오. 아래 결과가 나와야 함.
-# print(numbers ** 2)
-# print()
-# # f) floats 라는 이름의 칼럼을 추가하시오. 값은 1.5, 2.5, 3.5, 4.5    아래 결과가 나와야 함.
-# floats = [1.5, 2.5, 3.5, 4.5]
-# numbers['floats'] = floats
-# print(numbers)
-# # g) names라는 이름의 다음과 같은 칼럼을 위의 결과에 또 추가하시오. Series 클래스 사용.
-# names = pd.Series(['길동','오정','팔계','오공'], index=['d','a','b','c'])
-# print(names)
-# print()
-# numbers['names'] = names
-# print(numbers)
+print()
+# 2)
+# a) DataFrame으로 위와 같은 자료를 만드시오. colume(열) name은 numbers, row(행) name은 a~d이고 값은 10~40.
+numbers = pd.DataFrame(np.arange(10,50,10), columns=['numbers'], index=['a','b','c','d'])
+print(numbers)
+# b) c row의 값을 가져오시오.
+print(numbers.loc["c"])
+# c) a, d row들의 값을 가져오시오.
+print(numbers.iloc[0:4:3])
+# d) numbers의 합을 구하시오.
+print()
+print(numbers.sum())
+# e) numbers의 값들을 각각 제곱하시오. 아래 결과가 나와야 함.
+print(numbers ** 2)
+print()
+# f) floats 라는 이름의 칼럼을 추가하시오. 값은 1.5, 2.5, 3.5, 4.5    아래 결과가 나와야 함.
+floats = [1.5, 2.5, 3.5, 4.5]
+numbers['floats'] = floats
+print(numbers)
+# g) names라는 이름의 다음과 같은 칼럼을 위의 결과에 또 추가하시오. Series 클래스 사용.
+names = pd.Series(['길동','오정','팔계','오공'], index=['d','a','b','c'])
+print(names)
+print()
+numbers['names'] = names
+print(numbers)
 
 # pandas 문제 3)
 
@@ -45,7 +45,7 @@ from pandas import Series, DataFrame
 data = DataFrame(np.random.choice(range(1,21),15).reshape(5,3))
 print(data)
 # 2) 생성된 DataFrame의 컬럼 이름을 A, B, C로 설정하고, 행 인덱스를 r1, r2, r3, r4, r5로 설정하시오.
-col = ["A","B","c"]
+col = ["A","B","C"]
 row = ['r1','r2','r3','r4','r5']
 data.columns = col
 data.index = row
@@ -74,12 +74,14 @@ data = {
 da = pd.DataFrame(data, index=['p1','p2','p3','p4'])
 print(da)
 # 2) price와 stock을 이용하여 'total'이라는 새로운 컬럼을 추가하고, 값은 'price x stock'이 되도록 하시오.
-da.insert(3,'total',da['price'] * da['stock'])
+# da.insert(3,'total',da['price'] * da['stock'])
+# print(da)
+da['total'] = da['price'] * da['stock']
 print(da)
+print('-----00------')
 # 3) 컬럼 이름을 다음과 같이 변경하시오. 원본 갱신
 #    product → 상품명,  price → 가격,  stock → 재고,  total → 총가격
-col = ['상품명', '가격','재고','총가격']
-da.columns = col
+da.columns = ['상품명', '가격', '재고', '총가격']
 print(da)
 # 4) 재고(재고 컬럼)가 3 이하인 행의 정보를 추출하시오.
 print(da[da['재고'] <= 3])
@@ -87,7 +89,7 @@ print(da[da['재고'] <= 3])
 print('---' * 10)
 print(da.loc[['p2']])
 print('---' * 10)
-print(da.iloc[:1])
+print(da.iloc[[1]])
 # 6) 인덱스가 p3인 행을 삭제한 뒤, 그 결과를 확인하시오. (원본이 실제로 바뀌지 않도록, 즉 drop()의 기본 동작으로)
 imsi = da.drop(['p3'])
 print(imsi)
