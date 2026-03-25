42ea1bd4c74969b339a36b353a60e76933774a57
jinwonn <bluefrogg9@gmail.com>
2026-03-25 09:58:15 +0900

    문제 풀이 수정본

--- pro5anal/pandasdbquiz2.py
+++ pro5anal/pandasdbquiz2.py
@@ -1,7 +1,9 @@
 #  b) MariaDB에 저장된 jikwon 테이블을 이용하여 아래의 문제에 답하시오.
+#      - pivot_table을 사용하여 성별 연봉의 평균을 출력
+#      - 성별(남, 여) 연봉의 평균으로 시각화 - 세로 막대 그래프
+#      - 부서명, 성별로 교차 테이블을 작성 (crosstab(부서, 성별))
 
 import pymysql
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import koreanize_matplotlib
@@ -14,37 +16,51 @@ config = {
     'port': 3306,
     'charset': 'utf8'
 }
+
 try:
-    conn = pymysql.connect(**config)
-    cursor = conn.cursor() 
+    conn   = pymysql.connect(**config)
+    cursor = conn.cursor()
+
     sql = """
-        select jikwonno, jikwonname, busername, jikwonjik, jikwongen, jikwonpay from jikwon
-        inner join buser on jikwon.busernum = buser.buserno
-        """  
-    cursor.execute(sql) 
+        SELECT j.jikwonno   AS 사번,
+               j.jikwonname AS 직원명,
+               b.busername  AS 부서명,
+               j.jikwongen  AS 성별,
+               j.jikwonpay  AS 연봉
+        FROM   jikwon j
+        INNER JOIN buser b ON j.busernum = b.buserno
+    """
+    from sqlalchemy import create_engine
+    engine = create_engine("mysql+pymysql://root:123@127.0.0.1:3306/test")
+    df = pd.read_sql(sql, engine)
+    print(df.head(3))
 
-    rows = cursor.fetchall()
 
-    #   - pivot_table을 사용하여 성별 연봉의 평균을 출력
-    df3 = pd.DataFrame(rows, columns=['jikwonno', 'jikwonname', 'busername', 'jikwonjik','jikwongen', 'jikwonpay'])
-    gen_pay_pivot = df3.pivot_table(values='jikwonpay' , columns='jikwongen', aggfunc='mean')
-    print(gen_pay_pivot)
-    
-    #   - 성별(남, 여) 연봉의 평균으로 시각화 - 세로 막대 그래프
+    pt = pd.pivot_table(df, index='성별', values='연봉', aggfunc='mean')
+    print('성별 연봉 평균 (pivot_table)')
+    print(pt)
     print()
-    gen_pay_m = df3.groupby(['jikwongen'])['jikwonpay'].mean()  # 직급별 연봉
-    print(gen_pay_m)
 
-    plt.bar(gen_pay_m.index, gen_pay_m.values)
+
+    pt.plot(kind='bar', legend=False, color=['steelblue', 'coral'], rot=0)
+    plt.title('성별 평균 연봉')
+    plt.xlabel('성별')
+    plt.ylabel('연봉')
+    plt.tight_layout()
     plt.show()
 
-    #   - 부서명, 성별로 교차 테이블을 작성 (crosstab(부서명, 성별))
-    print()
-    ktab = pd.crosstab(df3['busername'], df3['jikwongen']) 
-    print('교차표\n' ,ktab)
 
+    ct = pd.crosstab(df['부서명'], df['성별'])
+    print('부서명 성별 교차 테이블')
+    print(ct)
+
+except pymysql.OperationalError as e:
+    print('DB 오류 :', e)
 except Exception as e:
-    print('처리오류 : ' , e)
+    print('처리 오류 :', e)
 finally:
-    cursor.close()
-    conn.close()
+    try:
+        cursor.close()
+        conn.close()
+    except:
+        pass
--- pro5anal/pandasdbquiz3.py
+++ pro5anal/pandasdbquiz3.py
@@ -6,51 +6,100 @@
 #     - 성별 연봉 분포 + 이상치 확인    <== 그래프 출력
 #     - Histogram (분포 비교) : 남/여 연봉 분포 비교    <== 그래프 출력
 
-import MySQLdb
-import numpy as np
+import pymysql
 import pandas as pd
 import matplotlib.pyplot as plt
 import koreanize_matplotlib
-import csv
+import seaborn as sns
+import os
 
 config = {
-    'host': '127.0.0.1',
-    'user': 'root',
-    'password': '123',
-    'database': 'test',
-    'port': 3306,
-    'charset': 'utf8'
+    'host':'127.0.0.1',
+    'password':'123',
+    'user':'root',
+    'database' : 'test',
+    'port':3306,
+    'charset':'utf8'
 }
+
 try:
-    conn = MySQLdb.connect(**config)
-    cursor = conn.cursor() 
-    sql = """
-        select jikwonno, jikwonname, busername, jikwonjik, jikwonpay, jikwongen, busertel from jikwon
-        inner join buser on jikwon.busernum = buser.buserno
-        """  
-    cursor.execute(sql) 
+    conn = pymysql.connect(**config)
+    cursor = conn.cursor()
+    sql="""
+        select 
+            jikwonno, jikwonname, busername, jikwonjik, busertel, jikwongen, jikwonpay, 
+            gogekno, gogekname, gogektel 
+        from jikwon
+        inner join buser on jikwon.busernum=buser.buserno
+        left outer join gogek on jikwon.jikwonno=gogek.gogekdamsano
+    """
 
-    rows = cursor.fetchall()
+    cursor.execute(sql)
+    df_raw = pd.DataFrame(cursor.fetchall(), columns=['사번', '이름', '부서명', '직급', '부서전화', '성별', '연봉', '고객번호', '고객명', '고객전화'])
+    df = df_raw.drop_duplicates(subset=['사번'])
+    
+except pymysql.OperationalError as e:
+    print(e)
 
-    df1 = pd.DataFrame(rows,
-                    columns= ['jikwonno', 'jikwonname', 'busername', 'jikwonjik', 'jikwonpay', 'jikwongen', 'busertel'])
-
-    with open('pandasdbquiz3.csv', mode='w', encoding='utf-8') as f:
-        writer = csv.writer(f)
-        for row in rows:
-            writer.writerow(row)
-
-    df2 = pd.read_csv('pandasdbquiz3.csv', header=None, names=['사번', '직원명', '부서명', '직급', '연봉', '성별', '부서전화'])
-
-    df = pd.read_sql(sql, conn) 
-    df.columns = ['사번', '직원명', '부서명', '직급', '연봉', '성별', '부서전화']
-
-     
-
-
-
-except MySQLdb.OperationalError as e:
-    print('처리오류 : ' , e)
 finally:
     cursor.close()
     conn.close()
+
+# print(df)
+# os._exit(0)
+
+#  c) 키보드로 사번, 직원명을 입력받아 로그인에 성공하면 console에 아래와 같이 출력하시오.
+#   조건 :  try ~ except MySQLdb.OperationalError as e:      사용
+#   사번  직원명  부서명   직급  부서전화  성별
+#   ...
+#   인원수 : * 명
+dfc = df[['사번', '이름', '부서명', '직급', '부서전화', '성별', '연봉']]
+dfc = dfc.rename(columns={'이름': '직원명'})
+# print(dfc)
+
+while True:
+    jikwonno = input('사번을 입력하세요. 종료:q\t')
+
+    if jikwonno == 'q':
+        break
+
+    if not jikwonno.isdigit(): 
+        print('사번은 숫자만 입력하세요\n')
+        continue
+
+    jikwonname = input('이름을 입력하세요. 종료:q\t')
+    if jikwonname == 'q':
+        break
+
+    if any(dfc['사번'] == int(jikwonno)):
+        name = dfc[dfc['사번'] == int(jikwonno)]['직원명'].iloc[0]
+        if name == jikwonname:
+            # 전직원 출력
+            print(dfc.drop(columns=['연봉'], axis=1))
+            print("인원수 : ", dfc['사번'].count(), "명")
+
+            male = dfc[dfc['성별']=='남']['연봉']
+            female = dfc[dfc['성별']=='여']['연봉']
+
+            figure, ((ax1,ax2),(ax3,ax4)) = plt.subplots(nrows=2, ncols=2)
+            figure.set_size_inches(15,10)
+
+            # - 성별 연봉 분포 + 이상치 확인    <== 그래프 출력
+            sns.boxplot(y=male, ax=ax1)
+            sns.boxplot(y=female, ax=ax2)
+
+            ax1.set(xlabel='남성', ylabel='연봉[원]', title='남성 연봉 분포')
+            ax2.set(xlabel='여성', ylabel='연봉[원]', title='여성 연봉 분포')
+
+            # - Histogram (분포 비교) : 남/여 연봉 분포 비교    <== 그래프 출력
+            sns.histplot(data=male, bins=10, ax=ax3)
+            sns.histplot(data=female, bins=10, ax=ax4)
+
+            ax3.set(xlabel='연봉[원]', ylabel='인원수[명]', title='남성 연봉 분포 비교')
+            ax4.set(xlabel='연봉[원]', ylabel='인원수[명]', title='여성 연봉 분포 비교')
+
+            plt.show()
+            break
+        else:
+            print("사번과 이름 정보가 일치하지 않습니다.\n")
+    else: print("존재하지 않는 사번 입니다.\n")
